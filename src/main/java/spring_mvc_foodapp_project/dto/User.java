75a71d775587e80b49399479ef1cec456daaab17
unknown <sourabhbellanki@gmail.com>
2023-03-21 17:24:09 +0530

    added dto

---
+++ src/main/java/spring_mvc_foodapp_project/dto/User.java
@@ -0,0 +1,94 @@
+package spring_mvc_foodapp_project.dto;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private String role;
+
+	@OneToOne
+	private Menu menu;
+	@OneToMany(mappedBy = "user")
+	List<Branch> branch;
+	@OneToMany(mappedBy = ="user")
+	List<FoodOrder> foodOrders;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public Menu getMenu() {
+		return menu;
+	}
+
+	public void setMenu(Menu menu) {
+		this.menu = menu;
+	}
+
+	public List<Branch> getBranch() {
+		return branch;
+	}
+
+	public void setBranch(List<Branch> branch) {
+		this.branch = branch;
+	}
+
+	public List<FoodOrder> getFoodOrders() {
+		return foodOrders;
+	}
+
+	public void setFoodOrders(List<FoodOrder> foodOrders) {
+		this.foodOrders = foodOrders;
+	}
+
+}
